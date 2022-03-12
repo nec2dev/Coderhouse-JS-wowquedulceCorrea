983011eb6df3c71efa0e54b9ce6159904b9611c0
Nahuel Correa <nec2multivackergithub@outlook.com>
2022-03-12 19:57:41 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme:{
+    screens:{
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px'
+    },
+    container:{
+      center: true,
+      padding: '1rem'
+    },
+    extend: {
+      fontFamily:{
+        poppins: "'Poppins', sans-serif",
+        roboto: "'Roboto', sans-serif"
+      },
+      colors:{ 
+        'primary': '#FD3057'
+      }
+    },
+  },
+   variants: {
+     extend: {
+       display: ['group-hover'],
+       visibility: ['group-hover']
+     },
+  },
+  plugins: [require('@tailwindcss/forms')],
+}
