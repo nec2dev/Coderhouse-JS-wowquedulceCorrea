76133fb50d14715b57c73dd03e0a062ef638e16c
Nahuel Correa <nec2multivackergithub@outlook.com>
2022-03-17 01:43:18 -0300

    Js ok

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,14 +1,21 @@
+const titulo = document.getElementById("titulo");
+const maincopy = document.getElementById("maincopy");
+
+const featuredboxes = ['box001', 'box002', 'box003', 'box004'];
+const featuredboxeslist = document.getElementById("featuredboxeslist");
+
 titulo.innerText = "Wow! qué dulce";
 maincopy.innerText = "Nuestras mejores boxes para regalarnos";
 
-const cargarproductos = ()=> {
+const cargarBoxesDestacadas = ()=> { 
     //debugger
-    for(box of boxes){
-        let itemList =
-             "<li class='collection-item'>" + box + "</li>"
-    boxeslist.innerHTML += itemList;
-    }
+    for (box of featuredboxes) {
+        const liBoxes = document.createElement("li")
+            liBoxes.className = "collection-item"
+            liBoxes.innerText = box
+            liBoxes.setAttribute("onclick", `agregarAlCarrito('${box}')`)
+                ul.append(liBoxes)
+            }
 }
 
-cargarproductos();
-
+cargarBoxesDestacadas();
--- src/assets/js/variables.js
+++ src/assets/js/variables.js
@@ -1,9 +1,7 @@
-const boxes = ['box001', 'box002', 'box003', 'box004', 'box005', 'box006'];
 const carrito = [];
 
-const titulo = document.getElementById("titulo");
-const maincopy = document.getElementById("maincopy");
 
-const boxeslist = document.getElementById("boxeslist");
+
+
 const kartlist = document.getElementById("kartlist");
 
