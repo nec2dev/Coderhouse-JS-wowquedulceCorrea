0a9bdb5edd63acce309a9560796867f8aaf0d2ca
Nahuel Correa <nec2multivackergithub@outlook.com>
2022-03-16 23:14:33 -0300

    variables

--- src/assets/js/variables.js
+++ src/assets/js/variables.js
@@ -1,4 +1,8 @@
-const boxes = ['box001', 'box002', 'box003', 'box004', 'box005', 'box 006'];
+const boxes = ['box001', 'box002', 'box003', 'box004', 'box005', 'box006'];
 const carrito = [];
 
-const titulo = document.getElementById("titulo")
+const titulo = document.getElementById("titulo");
+const maincopy = document.getElementById("maincopy");
+const ul = document.getElementById("boxeslist");
+const kartlist = document.getElementById("kartlist");
+
