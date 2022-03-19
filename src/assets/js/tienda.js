f4d0424ffb10f12922fdec7a3d9112c0a52b4777
Nahuel Correa <nec2multivackergithub@outlook.com>
2022-03-18 22:42:33 -0300

    modularizado de js Ok

--- src/assets/js/tienda.js
+++ src/assets/js/tienda.js
@@ -1,91 +1,91 @@
-const boxes = [
-  {
-      id: 001,
-      nombre: 'Wow! Box001',
-      precio: 560.00,
-      precioAnterior: 620.00,
-      imagen: '../assets/images/box001.jpg',
-      medidas: '15 x 15 x 10 cm',
-      peso: 3.20,
-      personas: 4,
-      numpiezas: 8,
-      piezas: ['Brownie', 'Lemon Pie', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de chocolate', 'Alfajorcito de chocolate'],
-      sabores: ['ch', 'li', 'v-ddl', 'v-ddl', 'v-ddl', 'v-ddl', 'ch-ddl', 'ch-ddl']
-  },
-  {
-      id: 002,
-      nombre: 'Wow! Box002',
-      precio: 940.50,
-      precioAnterior: 1020.00,
-      imagen: '../assets/images/box002.jpg',
-      medidas: '15 x 15 x 10 cm',
-      peso: 3.20,
-      personas: 4,
-      numpiezas: 8,
-      piezas: ['Brownie', 'Lemon Pie', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de chocolate', 'Alfajorcito de chocolate'],
-      sabores: ['ch', 'li', 'v-ddl', 'v-ddl', 'v-ddl', 'v-ddl', 'ch-ddl', 'ch-ddl']
-  },
-  {
-      id: 003,
-      nombre: 'Wow! Box003',
-      precio: 320.00,
-      precioAnterior: 420.00,
-      imagen: '../assets/images/box003.jpg',
-      medidas: '15 x 15 x 10 cm',
-      peso: 3.20,
-      personas: 4,
-      numpiezas: 8,
-      piezas: ['Brownie', 'Lemon Pie', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de chocolate', 'Alfajorcito de chocolate'],
-      sabores: ['ch', 'li', 'v-ddl', 'v-ddl', 'v-ddl', 'v-ddl', 'ch-ddl', 'ch-ddl']
-  },
-  {
-      id: 004,
-      nombre: 'Wow! Box004',
-      precio: 450.00,
-      precioAnterior: 620.00,
-      imagen: '../assets/images/box004.jpg',
-      medidas: '15 x 15 x 10 cm',
-      peso: 3.20,
-      personas: 4,
-      numpiezas: 8,
-      piezas: ['Brownie', 'Lemon Pie', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de chocolate', 'Alfajorcito de chocolate'],
-      sabores: ['ch', 'li', 'v-ddl', 'v-ddl', 'v-ddl', 'v-ddl', 'ch-ddl', 'ch-ddl']
-  },
-  {
-      id: 005,
-      nombre: 'Wow! Box005',
-      precio: 500.00,
-      precioAnterior: 620.00,
-      imagen: '../assets/images/box005.jpg',
-      medidas: '15 x 15 x 10 cm',
-      peso: 3.20,
-      personas: 4,
-      numpiezas: 8,
-      piezas: ['Brownie', 'Lemon Pie', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de chocolate', 'Alfajorcito de chocolate'],
-      sabores: ['ch', 'li', 'v-ddl', 'v-ddl', 'v-ddl', 'v-ddl', 'ch-ddl', 'ch-ddl']
-  },
-  {
-      id: 006,
-      nombre: 'Wow! Box006',
-      precio: 1028.00,
-      precioAnterior: 1320.00,
-      imagen: '../assets/images/box006.jpg',
-      medidas: '15 x 15 x 10 cm',
-      peso: 3.20,
-      personas: 4,
-      numpiezas: 8,
-      piezas: ['Brownie', 'Lemon Pie', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de chocolate', 'Alfajorcito de chocolate'],
-      sabores: ['ch', 'li', 'v-ddl', 'v-ddl', 'v-ddl', 'v-ddl', 'ch-ddl', 'ch-ddl']
-  }
+// const boxes = [
+//   {
+//       id: 001,
+//       nombre: 'Wow! Box001',
+//       precio: 560.00,
+//       precioAnterior: 620.00,
+//       imagen: '../assets/images/box001.jpg',
+//       medidas: '15 x 15 x 10 cm',
+//       peso: 3.20,
+//       personas: 4,
+//       numpiezas: 8,
+//       piezas: ['Brownie', 'Lemon Pie', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de chocolate', 'Alfajorcito de chocolate'],
+//       sabores: ['ch', 'li', 'v-ddl', 'v-ddl', 'v-ddl', 'v-ddl', 'ch-ddl', 'ch-ddl']
+//   },
+//   {
+//       id: 002,
+//       nombre: 'Wow! Box002',
+//       precio: 940.50,
+//       precioAnterior: 1020.00,
+//       imagen: '../assets/images/box002.jpg',
+//       medidas: '15 x 15 x 10 cm',
+//       peso: 3.20,
+//       personas: 4,
+//       numpiezas: 8,
+//       piezas: ['Brownie', 'Lemon Pie', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de chocolate', 'Alfajorcito de chocolate'],
+//       sabores: ['ch', 'li', 'v-ddl', 'v-ddl', 'v-ddl', 'v-ddl', 'ch-ddl', 'ch-ddl']
+//   },
+//   {
+//       id: 003,
+//       nombre: 'Wow! Box003',
+//       precio: 320.00,
+//       precioAnterior: 420.00,
+//       imagen: '../assets/images/box003.jpg',
+//       medidas: '15 x 15 x 10 cm',
+//       peso: 3.20,
+//       personas: 4,
+//       numpiezas: 8,
+//       piezas: ['Brownie', 'Lemon Pie', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de chocolate', 'Alfajorcito de chocolate'],
+//       sabores: ['ch', 'li', 'v-ddl', 'v-ddl', 'v-ddl', 'v-ddl', 'ch-ddl', 'ch-ddl']
+//   },
+//   {
+//       id: 004,
+//       nombre: 'Wow! Box004',
+//       precio: 450.00,
+//       precioAnterior: 620.00,
+//       imagen: '../assets/images/box004.jpg',
+//       medidas: '15 x 15 x 10 cm',
+//       peso: 3.20,
+//       personas: 4,
+//       numpiezas: 8,
+//       piezas: ['Brownie', 'Lemon Pie', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de chocolate', 'Alfajorcito de chocolate'],
+//       sabores: ['ch', 'li', 'v-ddl', 'v-ddl', 'v-ddl', 'v-ddl', 'ch-ddl', 'ch-ddl']
+//   },
+//   {
+//       id: 005,
+//       nombre: 'Wow! Box005',
+//       precio: 500.00,
+//       precioAnterior: 620.00,
+//       imagen: '../assets/images/box005.jpg',
+//       medidas: '15 x 15 x 10 cm',
+//       peso: 3.20,
+//       personas: 4,
+//       numpiezas: 8,
+//       piezas: ['Brownie', 'Lemon Pie', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de chocolate', 'Alfajorcito de chocolate'],
+//       sabores: ['ch', 'li', 'v-ddl', 'v-ddl', 'v-ddl', 'v-ddl', 'ch-ddl', 'ch-ddl']
+//   },
+//   {
+//       id: 006,
+//       nombre: 'Wow! Box006',
+//       precio: 1028.00,
+//       precioAnterior: 1320.00,
+//       imagen: '../assets/images/box006.jpg',
+//       medidas: '15 x 15 x 10 cm',
+//       peso: 3.20,
+//       personas: 4,
+//       numpiezas: 8,
+//       piezas: ['Brownie', 'Lemon Pie', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de maicena DDL', 'Alfajorcito de chocolate', 'Alfajorcito de chocolate'],
+//       sabores: ['ch', 'li', 'v-ddl', 'v-ddl', 'v-ddl', 'v-ddl', 'ch-ddl', 'ch-ddl']
+//   }
 
-];
+// ];
 
-let carrito = [];
-const moneda = '$';
-const DOMitems = document.querySelector('#items');
-const DOMcarrito = document.querySelector('#carrito');
-const DOMtotal = document.querySelector('#total');
-const DOMbotonVaciar = document.querySelector('#boton-vaciar');
+// let carrito = [];
+// const moneda = '$';
+// const DOMitems = document.querySelector('#items');
+// const DOMcarrito = document.querySelector('#carrito');
+// const DOMtotal = document.querySelector('#total');
+// const DOMbotonVaciar = document.querySelector('#boton-vaciar');
 
 function listarBoxes() {
   //debugger
