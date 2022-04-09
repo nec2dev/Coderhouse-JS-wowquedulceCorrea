153b78d2758bce1efe27cb1fbf4fc65396386030
Nahuel Correa <nec2multivackergithub@outlook.com>
2022-04-09 20:15:03 -0300

    función que tras 2 seg retorna un catalogo de prod

--- src/assets/js/localstorage.js
+++ src/assets/js/localstorage.js
@@ -373,12 +373,30 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
     DOMbotonVaciar.addEventListener("click", vaciarCarrito);
-    
+
+  // función que tras 2 segundo retorna un array de objetos
+  const pedirProductos = () => {
+    return new Promise( (resolve, reject) => {
+        setTimeout(() => {
+            resolve(boxes)
+        }, 2000)
+    })
+  }
+
+// inicializamos con un array vacío
+let productos = []
+const renderProductos = (arr) => {
+  listarBoxes();
+}
+// asincrónicamente pedimos los datos y generamos la vista
+pedirProductos()
+    .then((res) => {
+        productos = res
+        renderProductos(productos)
+    })
 
   //Eventos
   agregarCarritoDeLocalStorage();
-  listarBoxes();
   mostrarCarrito();
-
 });
 
